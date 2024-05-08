37c814a4353a872abb42cd091241a83f3570ba6f
Junn28 <juniarda28@gmail.com>
2024-05-08 20:38:34 +0700

    create project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Absen Kuy!</title>
+
+    <!-- styles -->
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        h1 {
+            color: salmon;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Hello World!!!</h1>
+</body>
+
+</html>
